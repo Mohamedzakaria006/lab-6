4abd6522637e7b3f0a142a748f74014342560dee
= <=>
2024-02-27 17:55:00 +0200

    lab-6

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,12 @@ import { AdminComponent } from '../components/admin/admin.component';
 import { InsertproductsComponent } from '../components/admin/insertproducts/insertproducts.component';
 import { CartComponent } from '../components/cart/cart.component';
 import { HomeComponent } from '../components/home/home.component';
+import { authGuard } from './Guard/auth.guard';
+import { RegisterComponent } from '../components/register/register.component';
+import { LoginComponent } from '../components/login/login.component';
 
 export const routes: Routes = [
+  /*
   {
     path: '',
     component: HomeComponent,
@@ -17,5 +21,23 @@ export const routes: Routes = [
   {
     path: 'cart',
     component: CartComponent,
+  },
+  */
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'cart', component: CartComponent },
+
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    children: [
+      {
+        path: 'addProduct',
+        component: InsertproductsComponent,
+        canActivate: [authGuard],
+      },
+    ],
   },
 ];
